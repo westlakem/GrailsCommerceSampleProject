5c7b83124cf6563c50372329a381fdb1b76854a9
Matt Westlake <westlake_m@yahoo.com>
2015-01-16 08:17:30 -0500

    build - added bootstrap launch to add categories and products in development

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import shoppingsolutionproject.Category;
+import shoppingsolutionproject.Item
+
 class BootStrap {
+	
+	def dataSource
 
     def init = { servletContext ->
+		def car = new Category(description: 'Car').save()
+		def truck = new Category(description: 'Truck').save()
+		new Item(productNumber:1, name:"First Product", description: "This is the first product I'm adding for testing",
+				 shippingCost: 4.99, url: '/first', retailPrice: 19.99, salePrice: 16.99, category: car).save(failOnError: true)
+		new Item(productNumber:2, name:"Second Product", description: "This is the second product I'm adding for testing",
+				shippingCost: 4.99, url: '/second', retailPrice: 19.99, category: truck).save(failOnError: true)
+		new Item(productNumber:3, name:"First/Second Product", description: "This is the first/second(so third) product I'm adding for testing",
+				shippingCost: 4.99, url: '/third', retailPrice: 17.99, category: truck).save(failOnError: true)
     }
     def destroy = {
     }
--- grails-app/views/products/_searchBar.gsp
+++ grails-app/views/products/_searchBar.gsp
@@ -1,5 +1,5 @@
 <div class="right">
 	<g:form action="list">
-		<g:textField size="40" name="searchParam" placeholder="Product Number, Name, or Description"/><g:actionSubmit value="Search"/>
+		<g:textField size="40" name="searchParam" placeholder="Product Number, Name, or Description"/><g:submitButton name="submit" value="Search"/>
 	</g:form>
 </div>
